ade63f92c8274f2ef3c398132ba97d88afdc7649
Richard Tibor Fober <fober@usc.edu>
2025-04-25 04:28:57 +0000

    testing before submitting

--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(
+    string curr,
+    const string& in,
+    string floating,
+    const set<string>& dict,
+    set<string>& results,
+    size_t index);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +28,48 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> results;
+    wordleHelper("", in, floating, dict, results, 0);
+    return results;
 }
 
 // Define any helper functions here
+void wordleHelper(
+    string curr,
+    const string& in,
+    string floating,
+    const set<string>& dict,
+    set<string>& results,
+    size_t index)
+{
+    if(index == in.size()) // base case 1
+    {
+        if(floating.empty() && dict.find(curr) != dict.end())
+        {
+            results.insert(curr);
+        }
+        return;
+    }
+
+    if(in[index] != '-')
+    {
+        wordleHelper(curr + in[index], in, floating, dict, results, index + 1);
+    }
+    else
+    {
+        // try all 26 letters in alphabet
+        for(char c = 'a'; c <= 'z'; ++c)
+        {
+            string newFloating = floating;
+
+            size_t pos = newFloating.find(c);
+            if(pos != string::npos)
+            {
+                newFloating.erase(pos, 1);
+
+            }
+
+            wordleHelper(curr + c, in, newFloating, dict, results, index + 1);
+        }
+    }
+}
