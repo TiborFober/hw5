2f89240c030e3a2c931ab8b1cd9c2be2f13be088
Richard Tibor Fober <fober@usc.edu>
2025-04-25 21:25:43 +0000

    testing schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,19 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    size_t dailyNeed,
+    size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& shifts,
+    size_t day,
+    size_t slot );
 
+bool alreadyScheduledToday(const vector<Worker_T>& daySchedule, Worker_T worker)
+{
+    return find(daySchedule.begin(), daySchedule.end(), worker) != daySchedule.end();
+}
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -32,14 +44,59 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.size() == 0){
         return false;
     }
     sched.clear();
     // Add your code below
 
+    size_t n = avail.size();
+    size_t k = avail[0].size();
 
+    sched.resize(n, vector<Worker_T>(dailyNeed, -1));
 
+    vector<int> shifts(k, 0);
 
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, 0, 0);
+}
+
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    size_t dailyNeed,
+    size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& shifts,
+    size_t day,
+    size_t slot )
+{
+    if(day == sched.size())
+    {
+        return true;
+    }
+
+    if(slot == dailyNeed)
+    {
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, day + 1, 0);
+    }
+
+    for(size_t worker = 0; worker < avail[0].size(); ++worker)
+    {
+        if(avail[day][worker] && shifts[worker] < maxShifts && !alreadyScheduledToday(sched[day], worker))
+        {
+            sched[day][slot] = worker;
+            shifts[worker]++;
+
+            if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, day, slot + 1 ))
+            {
+                return true;
+            }
+
+            shifts[worker]--;
+            sched[day][slot] = -1;
+            
+        }
+    }
+
+    return false;
 }
 
